05ee3d805b388c0736c9b365275a373d3fa2db78
ciscon <dustin.glidden@gmail.com>
2020-01-15 19:41:24 -0500

    working config

--- extended_theme.lua
+++
@@ -1,9 +0,0 @@
-dofile(os.getenv("HOME") .. "/.config/awesome/themes/zenburn/theme.lua")
-theme.layout_termfair      = os.getenv("HOME") .. "/.config/awesome/vain/themes/zenburn/layouts/termfairw.png"
-theme.layout_browse        = os.getenv("HOME") .. "/.config/awesome/vain/themes/zenburn/layouts/browsew.png"
-theme.layout_gimp          = os.getenv("HOME") .. "/.config/awesome/vain/themes/zenburn/layouts/gimpw.png"
-theme.layout_cascade       = os.getenv("HOME") .. "/.config/awesome/vain/themes/zenburn/layouts/cascadew.png"
-theme.layout_cascadebrowse = os.getenv("HOME") .. "/.config/awesome/vain/themes/zenburn/layouts/cascadebrowsew.png"
-theme.layout_centerwork    = os.getenv("HOME") .. "/.config/awesome/vain/themes/zenburn/layouts/centerworkw.png"
-
-return theme
