2b6c5d2e78d10e521ce159f089a540b965818086
ciscon <ciscon@nicotinelounge.com>
2023-06-04 15:49:53 -0400

    better spacing

--- rc.lua
+++ rc.lua
@@ -85,7 +85,9 @@ awful.button({ modkey }, 3, function(t)
   end
 end),
 awful.button({ }, 4, function() tag_view_nonempty_fixed(1) end),
-awful.button({ }, 5, function() tag_view_nonempty_fixed(-1) end)
+awful.button({ }, 5, function() tag_view_nonempty_fixed(-1) end),
+awful.button({ modkey }, 4,  function(t) awful.tag.viewnext(t.screen) end),
+awful.button({ modkey }, 5,  function(t) awful.tag.viewprev(t.screen) end)
 )
 
 
@@ -152,7 +154,7 @@ screen.connect_signal("property::geometry", set_wallpaper)
 awful.screen.connect_for_each_screen(function(s)
   set_wallpaper(s)
 
-  awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+  awful.tag({ "1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 ", "8 ", "9 " }, s, awful.layout.layouts[1])
 
   s.mypromptbox = awful.widget.prompt()
   s.mylayoutbox = awful.widget.layoutbox(s)
@@ -456,23 +458,23 @@ awful.screen.connect_for_each_screen(function(s)
    }, properties = { floating = true }},
 
    { rule = { class = "Thunderbird" },
-   properties = { screen = screen.count(), tag = "1" } },
+   properties = { screen = screen.count(), tag = "1 " } },
    { rule = { class = "discord" },
-   properties = { screen = 1, tag = "7" } },
+   properties = { screen = 1, tag = "7 " } },
    { rule = { class = "Hexchat" },
-   properties = { screen = 1, tag = "8" } },
+   properties = { screen = 1, tag = "8 " } },
    { rule = { class = "Deadbeef" },
-   properties = { screen = 1, tag = "9" } },
+   properties = { screen = 1, tag = "9 " } },
    { rule = { class = "steam" },
-   properties = { screen = 1, tag = "5" } },
+   properties = { screen = 1, tag = "5 " } },
    { rule = { class = "Steam" },
-   properties = { screen = 1, tag = "5" } },
+   properties = { screen = 1, tag = "5 " } },
    { rule = { class = "steamwebhelper" },
-   properties = { screen = 1, tag = "5" } },
+   properties = { screen = 1, tag = "5 " } },
    { rule = { class = "Slack" },
-   properties = { screen = 1, tag = "6" } },
+   properties = { screen = 1, tag = "6 " } },
    { rule_any = { class = { "youtubemusic" } },
-   properties = { screen = 1, tag = "9", floating = false } },
+   properties = { screen = 1, tag = "9 ", floating = false } },
  }
 
 
