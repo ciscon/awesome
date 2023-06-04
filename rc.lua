6df3997ee5843d32a299ddcffafa3a34a40d6ea8
ciscon <ciscon@nicotinelounge.com>
2023-06-03 23:04:50 -0400

    wallpaper

--- rc.lua
+++ rc.lua
@@ -141,8 +141,23 @@ local tray = wibox.widget {
   widget = wibox.container.place,
 }
 
+local function set_wallpaper(s)
+  -- Wallpaper
+  if beautiful.wallpaper then
+    local wallpaper = beautiful.wallpaper
+    -- If wallpaper is a function, call it with the screen
+    if type(wallpaper) == "function" then
+      wallpaper = wallpaper(s)
+    end
+    gears.wallpaper.maximized(wallpaper, s, true)
+  end
+end
+
+-- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)                                                        
+screen.connect_signal("property::geometry", set_wallpaper)
 
 awful.screen.connect_for_each_screen(function(s)
+  set_wallpaper(s)
 
   awful.tag({ " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 " }, s, awful.layout.layouts[1])
 
@@ -244,7 +259,6 @@ awful.screen.connect_for_each_screen(function(s)
    {description = "jump to urgent client", group = "client"}),
    awful.key({ modkey,           }, "Escape",
    function ()
-
      awful.client.focus.history.previous()
      if client.focus then
        client.focus:raise()
@@ -384,7 +398,7 @@ awful.screen.connect_for_each_screen(function(s)
    end),
    awful.button({ modkey }, 3, function (c)
      c:emit_signal("request::activate", "mouse_click", {raise = true})
-     awful.mouse.client.resize(c)
+     if awful.client.floating.get(c) then awful.mouse.client.resize(c) end
    end)
    )
 
