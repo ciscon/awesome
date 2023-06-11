28d36211733a6b210539d5de06fb5d1b8d0aaed8
ciscon <ciscon@nicotinelounge.com>
2023-06-11 01:58:43 -0400

    colors

--- rc.lua
+++ rc.lua
@@ -129,7 +129,7 @@ function widget_loadavg(format)
   local n = f:read()
   f:close()
   local pos = n:find(' ', n:find(' ', n:find(' ')+1)+1)
-  loadavg_widget:set_markup('<span foreground="'..beautiful.border_focus..'">'..n:sub(1,pos-10)..'</span>')
+  loadavg_widget:set_markup('<span foreground="'..'#83412d'..'">'..n:sub(1,pos-10)..'</span>')
 end
 
 local loadavg_timer = require("gears.timer")
@@ -225,8 +225,7 @@ awful.screen.connect_for_each_screen(function(s)
   { -- Right widgets
   layout = wibox.layout.fixed.horizontal,
   spacerempty,
-  wibox.container.margin(tray,0,0,2,2),
-  spacerempty,
+  wibox.container.margin(tray,5,5,2,2),
   spacerempty,
   loadavg_widget,
   spacerempty,
